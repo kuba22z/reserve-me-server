6a7ebbd2a68de17258d8a68eaed00813e5ae0837
kuba22z <kuba6330@gmail.com>
2024-03-13 20:13:53 +0100

    adjust schema.prisma and create fake data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,69 @@
 import { PrismaClient } from '@prisma/client'
-import { fakeClient } from '../types/fake-data'
+import generateFakeData from './fake-data2'
+import { ITXClientDenyList } from '@prisma/client/runtime/library'
 
 const prisma = new PrismaClient()
 
-async function initializeDatabase() {
-  await prisma.client.upsert({
+async function initializeDatabase(tx: Omit<PrismaClient, ITXClientDenyList>) {
+  /*  await prisma.client.upsert({
     where: { id: 1 },
     create: fakeClient(),
     update: {},
-  })
+  })*/
 
   //const addUsers = async () => await prisma.user.createMany({ data: users });
+
+  const fakeData = generateFakeData()
+
+  await tx.employee.createMany({
+    data: fakeData.employees,
+  })
+
+  await tx.location.createMany({
+    data: fakeData.locations,
+  })
+
+  await tx.employeeSchedule.createMany({
+    data: fakeData.employeeSchedules,
+  })
+
+  await tx.meetingSchedule.createMany({
+    data: fakeData.meetingSchedules,
+  })
+
+  await tx.meeting.createMany({
+    data: fakeData.meetings,
+  })
+
+  await tx.client.createMany({
+    data: fakeData.clients,
+  })
+
+  await tx.clientsOnMeetings.createMany({
+    data: fakeData.clientsOnMeetings,
+  })
+
+  await tx.service.createMany({
+    data: fakeData.services,
+  })
+
+  await tx.servicesProvidedOnMeetings.createMany({
+    data: fakeData.servicesProvidedOnMeetings,
+  })
+
+  await tx.servicesBookedOnMeetings.createMany({
+    data: fakeData.servicesBookedOnMeetings,
+  })
+  //const addUsers = async () => await prisma.user.createMany({ data: users });
+}
+const trans = async () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return prisma.$transaction(async (tx) => initializeDatabase(tx))
 }
 
-initializeDatabase()
+trans()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .then((_) => console.log('Database seeded successfully!'))
   .catch((e) => {
     console.error(e)
     process.exit(1)
