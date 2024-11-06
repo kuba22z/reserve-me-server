dec2f228af0346b7acf9f3aa57b8957a9c8c4544
kuba22z <kuba6330@gmail.com>
2024-11-06 17:38:06 +0100

    use deploy simple node js server

---
+++ src/main2.js
@@ -0,0 +1,21 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  // Set the response HTTP headers
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+
+  // Send a response based on the request URL
+  if (req.url === '/') {
+    res.end('Hello, world! Welcome to the homepage.')
+  } else if (req.url === '/about') {
+    res.end('This is the about page.')
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('404 Not Found')
+  }
+})
+
+// Start the server and listen on the specified port
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running at http://localhost:${process.env.PORT}`)
+})
