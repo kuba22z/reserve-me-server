d656230366470002205e98f6471b3eb6c5e1a4e7
kuba22z <kuba6330@gmail.com>
2024-11-06 19:59:08 +0100

    try fix pipeline

--- src/main2.js
+++ src/main2.js
@@ -1,21 +1,12 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  // Set the response HTTP headers
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-
-  // Send a response based on the request URL
-  if (req.url === '/') {
-    res.end('Hello, world! Welcome to the homepage.')
-  } else if (req.url === '/about') {
-    res.end('This is the about page.')
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' })
-    res.end('404 Not Found')
-  }
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World\n')
 })
 
-// Start the server and listen on the specified port
-server.listen(3000, () => {
-  console.log(`Server is running at http://localhost:${3000}`)
+const PORT = process.env.PORT ?? "8080"
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`)
 })
